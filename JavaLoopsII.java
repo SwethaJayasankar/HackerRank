5fad5bcad26f2338c1d242fdfb7e1f1c980821d8
SwethaJayasankar <137705326+SwethaJayasankar@users.noreply.github.com>
2025-05-21 10:25:35 +0530

    Create JavaLoopsII.java

---
+++ JavaLoopsII.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class JavaLoopsII {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt(); // number of queries
+
+        for (int i = 0; i < q; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int result = a;
+            for (int j = 0; j < n; j++) {
+                result += Math.pow(2, j) * b;
+                System.out.print(result + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
